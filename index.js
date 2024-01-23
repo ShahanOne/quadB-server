cfc90b3b8259c5f5b6c4c463620429e69cf16a1d
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-01-23 22:29:55 +0530

    server set up

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const mongoose = require('mongoose');
+require('mongoose-type-url');
+mongoose.set('strictQuery', false);
+
+//Connection
+mongoose.connect(process.env.MONGODB_URI);
+
+//Schema
+const infoSchema = new mongoose.Schema({
+  name: String,
+  last: Number,
+  buy: Number,
+  sell: Number,
+  volume: Number,
+  base_unit: String,
+});
+
+//Model
+const Info = mongoose.model('Info', infoSchema);
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.wazirx.com/api/v2/tickers');
+    const data = Object.entries(response.data).slice(0, 10);
+
+    const dataArray = data.map(([key, value]) => ({ ...value, name: key }));
+
+    {
+      /*Called only once to store only 10 data */
+    }
+    // await Info.insertMany(dataArray);
+
+    res.json(Object.fromEntries(dataArray));
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
